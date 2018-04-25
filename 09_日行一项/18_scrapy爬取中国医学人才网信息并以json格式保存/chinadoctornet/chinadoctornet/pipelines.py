caaeaf21e1e4b397fdb446cd42705c9e9aa26cbb
jiguang123 <1528840916@qq.com>
2018-04-25 19:41:58 +0800

    48th commit

--- 09_日行一项/18_scrapy爬取中国医学人才网信息并以json格式保存/chinadoctornet/chinadoctornet/pipelines.py
+++ 09_日行一项/18_scrapy爬取中国医学人才网信息并以json格式保存/chinadoctornet/chinadoctornet/pipelines.py
@@ -4,21 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import json
+# import json
 
 class ChinadoctornetPipeline(object):
     def process_item(self, item, spider):
         return item
 
 
-class JsonWriterPipeline(object):
-    def __init__(self):
-        self.file = open('中国医学人才网招聘最新招聘专栏2.json', 'w', encoding='utf-8')
+# class JsonWriterPipeline(object):
+#     def __init__(self):
+#         self.file = open('中国医学人才网招聘最新招聘专栏2.json', 'w', encoding='utf-8')
 
-    def process_item(self, item, spider):
-        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
-        self.file.write(line)
-        return item
+#     def process_item(self, item, spider):
+#         line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+#         self.file.write(line)
+#         return item
 
-    def spider_closed(self, spider):
-        self.file.close()
+#     def spider_closed(self, spider):
+#         self.file.close()
--- 09_日行一项/18_scrapy爬取中国医学人才网信息并以json格式保存/chinadoctornet/chinadoctornet/settings.py
+++ 09_日行一项/18_scrapy爬取中国医学人才网信息并以json格式保存/chinadoctornet/chinadoctornet/settings.py
@@ -64,10 +64,10 @@ COOKIES_ENABLED = False
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   # 'chinadoctornet.pipelines.ChinadoctornetPipeline': 300,
-     'chinadoctornet.pipelines.JsonWritePipline': 300,
-}
+# ITEM_PIPELINES = {
+#    # 'chinadoctornet.pipelines.ChinadoctornetPipeline': 300,
+#      'chinadoctornet.pipelines.JsonWritePipline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
